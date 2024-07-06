f40932067077ef73bbc1281360a1984495e34766
Renan Costa Alencar <renan.costaalencar@gmail.com>
2024-07-06 15:57:17 -0300

    feat: :sparkles: Criando o model Mensagem

--- api_golang_ia/controllers/home_controller.go
+++ api_golang_ia/controllers/home_controller.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"api_golang_ia/models"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,7 +10,7 @@ type HomeController struct {
 }
 
 func (hc HomeController) Index(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "Esta é uma API construída na Live Code The Future Evento DIO",
+	c.JSON(200, models.Mensagem{
+		Mensagem: "Esta é uma API construída na Live Code The Future Evento DIO",
 	})
 }
